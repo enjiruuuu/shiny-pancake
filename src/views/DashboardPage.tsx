930eade4ce875c6d5fe57f860414ba25b96a4787
enjiruuuu <fightingdreamer99@gmail.com>
2022-05-16 20:12:24 +0800

    add some dashboard components

--- src/views/DashboardPage.tsx
+++ src/views/DashboardPage.tsx
@@ -1,10 +1,13 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import Header from "../components/Header";
+import Wrapper from "../components/Wrapper";
 import LoginHelper from "../helpers/LoginHelper";
 import NavigationHelper from "../helpers/Navigator";
+import Constants from "../models/Constants";
 
 const DashboardPage = () => {
-    const  navigator: NavigationHelper = new NavigationHelper();
+    const navigator: NavigationHelper = new NavigationHelper();
+    const userName: string = window.sessionStorage.getItem(Constants.namespace + '_userName') as string;
 
     useEffect(() => {
         if (!LoginHelper.checkSession()) {
@@ -13,7 +16,18 @@ const DashboardPage = () => {
     });
 
     return (
-        <Header></Header>
+        <Wrapper>
+            <Header></Header>
+            <main>
+                <h1>Hello, {userName}! 👋</h1>
+                <div>
+                    <div>
+                        <h2>Your trips</h2>
+                        <button className="tertiary">Create new trip</button>
+                    </div>
+                </div>
+            </main>
+        </Wrapper>
     );
 }
 
