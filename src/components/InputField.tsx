2c79d9e7370a592364eab644356829a3986c9f8b
enjiruuuu <fightingdreamer99@gmail.com>
2022-06-01 02:53:53 +0800

    add fields to create new trip

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -5,7 +5,7 @@ const InputField: React.FC<IInputField> = (props) => {
     return (
         <div className="c_inputField">
             <label>{props.label}</label>
-            <input type={props.type} placeholder={props.placeholder} required={props.required}></input>
+            <input type={props.type} placeholder={props.placeholder} required={props.required} onChange={props.onChange}></input>
             <span className="error_message">{props.error}</span>
         </div>
     );
